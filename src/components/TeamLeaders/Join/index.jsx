4a3fd3f7f7321cabc1f936de21712f4f088ae095
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-12-01 17:48:56 +0800

    team leaders

--- src/components/TeamLeaders/Join/index.jsx
+++ src/components/TeamLeaders/Join/index.jsx
@@ -6,16 +6,30 @@ export default function Join() {
   const history = useHistory();
 
   return (
-    <Container className="--max">
+    <Container className="--max join-our-team position-relative">
+      <Image
+        className="svg svg--4"
+        src={require("../../../assets/images/teamleaders/svg-3.svg").default}
+      />
       <Row>
-        <Col className="join-image">
+        <Col
+          xs={12}
+          className="join-image d-flex justify-content-center position-relative"
+        >
           <Image
+            className="svg svg--3"
+            src={
+              require("../../../assets/images/teamleaders/Shape.svg").default
+            }
+          />
+          <Image
+            className="join-image__image"
             src={
               require("../../../assets/images/teamleaders/Image 78.png").default
             }
           />
         </Col>
-        <Col>
+        <Col className="d-flex flex-column justify-content-center align-items-center text-center">
           <h1 data-scroll data-scroll-speed="1" className="--title">
             Join Our Team
           </h1>
@@ -25,19 +39,21 @@ export default function Join() {
           </h6>
           <p data-scroll data-scroll-speed="2" className="--desc">
             From our humble start, we have grown tremendously and <br />
-            now we are looking to go next level. We have a no- <br />
-            discrimination policy and believe in attitude, not so much <br />
-            aptitude. Of course, hard work is rewarded, excellence is <br />
-            our lifestyle. If you're energetic, eager to learn and a team <br />
-            player, send us your resume and you could be on your way to <br />a
-            career with Temokin Holdings.
+            now we are looking to go next level. We have <br />a no-
+            discrimination policy and believe in attitude, not so <br /> much
+            aptitude. Of course, hard work is rewarded, and <br />
+            excellence is our lifestyle. If you're energetic, eager to learn{" "}
+            <br />
+            and a team player, send us your resume and you could be on
+            <br /> your way to a career with Temokin Holdings.
           </p>
           <Button
-            onClick={() => history.push("/career")}
             data-scroll
             data-scroll-speed="2"
             className="--btn-send-resume"
-          />
+          >
+            <NavLink to="/career">Send Resume</NavLink>
+          </Button>
         </Col>
       </Row>
     </Container>

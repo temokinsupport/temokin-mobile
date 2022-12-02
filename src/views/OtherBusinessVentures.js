0bdddcd2578c68e368602aec20b572545a995616
byronjester.manalo@gmail.com <byronjester.manalo@gmail.com>
2022-12-02 08:59:18 +0800

    Other Business and Ventures Mobile View

--- src/views/OtherBusinessVentures.js
+++ src/views/OtherBusinessVentures.js
@@ -1,6 +1,7 @@
 import "../assets/styles/otherbusinessventures.scss";
 import useLocoScroll from "../components/hooks/useLocoScroll";
 import Navigation from "../components/Navigation";
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
 // import FooterMenu from "../components/FooterMenu";
 import Footer from "../components/Footer";
 
@@ -17,11 +18,18 @@ const OtherBusinessVentures = () => {
           <Navigation />
           <section className=" hero">
             <div className="hero__content">
-              <h1 className="hero__title">Other Business Affiliates</h1>
+              <h1 className="hero__title">Other Business</h1>
+              <h1 className="hero__title">Affiliates</h1>
+
               <p className="hero__desc">coming soon</p>
+              <Image
+              className="--image"
+              src={require("../assets/images/otherbusinessventures/Group_1091.svg").default}
+            />
             </div>
           </section>
-          <Footer />
+            
+          <Footer /> 
         </div>
       }
     </>

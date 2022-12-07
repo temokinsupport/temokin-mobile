21c84ef7e6ed9ba3ed91f206e532a7ce77bcdc9c
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-12-08 00:15:48 +0800

    remove is reveal

--- src/components/About/WeDig/index.jsx
+++ src/components/About/WeDig/index.jsx
@@ -15,10 +15,8 @@ export default function WeDig() {
       <Container className="d-flex h-100 justify-content-center align-items-center">
         <Row>
           <Col>
-            <h1 className="--title" data-scroll>
-              We Dig
-            </h1>
-            <p className="--desc" data-scroll>
+            <h1 className="--title">We Dig</h1>
+            <p className="--desc">
               More than just our values, we dig is a tribute <br />
               to our early pipe jacking and earthworks days,
               <br />
